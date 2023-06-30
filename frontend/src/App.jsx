1c470b91e07475f3523091dd2baa27484b01025f
Saied Sani <saied.sani.biderooni@gmail.com>
2023-06-30 21:05:39 +0200

    Create-Git-Repository

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>list Products</main>
+      <footer>All Rights Reserved</footer>
+    </div>
+  );
+}
+
+export default App;

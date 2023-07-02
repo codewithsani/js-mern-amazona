b659d09b5e59d050c95b9d01178dac5a769ff46e
Saied Sani <saied.sani.biderooni@gmail.com>
2023-07-02 14:37:06 +0200

    Create-Payment-Method-Screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     ctxDispatch({ type: "USER_SIGNOUT" });
     localStorage.removeItem("userInfo");
     localStorage.removeItem("shippingAddress");
+    localStorage.removeItem("paymentMethod");
   };
   return (
     <div className="d-flex flex-column site-container">
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,8 +16,9 @@ import { HelmetProvider } from "react-helmet-async";
 import { StoreProvider } from "./Store";
 import CartScreen from "./screens/CartScreen";
 import SigninScreen from "./screens/SigninScreen";
-import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SignupScreen from "./screens/SignupScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 
 axios.defaults.baseURL = import.meta.env.DEV ? "http://localhost:5000" : "/";
 
@@ -28,8 +29,9 @@ const router = createBrowserRouter(
       <Route path="product/:slug" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/signin" element={<SigninScreen />} />
-      <Route path="/shipping" element={<ShippingAddressScreen />} />
       <Route path="/signup" element={<SignupScreen />} />
+      <Route path="/shipping" element={<ShippingAddressScreen />} />
+      <Route path="/payment" element={<PaymentMethodScreen />} />
       {/* <Route path="dashboard" element={<Dashboard />} /> */}
       {/* ... etc. */}
     </Route>
